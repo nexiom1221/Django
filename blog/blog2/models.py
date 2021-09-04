5bdb71f86c26f9ab33e21dd3dccea480eccd1b51
nexiom1221 <superdizmon1@gmail.com>
2021-09-04 19:07:31 +0900

    CRUD 기능1

--- blog/blog2/models.py
+++ blog/blog2/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
 from taggit.managers import TaggableManager
+from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 class Post(models.Model):
     title = models.CharField(verbose_name='TITLE', max_length=50)
@@ -10,6 +12,7 @@ class Post(models.Model):
     create_dt = models.DateTimeField('CREATE DATE', auto_now_add=True)
     modify_dt = models.DateTimeField('MODIFY DATE', auto_now=True)
     tags = TaggableManager(blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='OWNER', blank=True, null=True)
 
     class Meta:
         verbose_name = 'post'
